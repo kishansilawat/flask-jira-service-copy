2663caabe823a843189c52143403950da3f7ec08
Kishan Silawat <kishansilawat@gmail.com>
2020-02-15 17:14:37 +0530

    Integrated SQL Alchemy with Postgres

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-if __name__  == '__main__':
-	app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
